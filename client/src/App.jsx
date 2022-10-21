d7bf49dd25117aec59bca6afd9941246ba674a3d
Marpureddy Saswat <saswatmarpureddy@gmail.com>
2022-10-21 14:26:49 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Footer, Navbar, Services, Transactions, Welcome } from "./components";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+}
---
+++ client/src/utils/constants.js
@@ -0,0 +1,3 @@
+import abi from "./Transactions.json";
+export const contractABI = abi.abi;
+export const contractAddress = "0xCF384fD1A4EB98cDcdEb692951Dc21C01a8dF98c";
